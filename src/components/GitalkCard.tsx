786ce485658166268347cba233c994642f028890
wjsoj <wjs@wjsphy.top>
2024-08-26 21:43:25 +0800

    fix: disable SSR

--- src/components/GitalkCard.tsx
+++ src/components/GitalkCard.tsx
@@ -1,19 +1,23 @@
 import GitalkComponent from "gitalk/dist/gitalk-component";
-import { usePageData } from 'rspress/runtime';
+import { usePageData,NoSSR } from 'rspress/runtime';
 
 export default function Gitalk() {
   const pageData = usePageData();
   return (
-    <GitalkComponent
-      options={{
-        clientID: "Ov23liBtIq74Cltq7uJQ",
-        clientSecret: 'e767cb1c64a63d6cc4f812bde6394836f02cee9c',
-        id: pageData.page.pagePath,
-        repo: "Lecture-Site",
-        owner: "wjsoj",
-        admin: ["wjsoj"],
-        title: pageData.page.title,
-      }}
-    />
+    <NoSSR>
+    {
+      pageData.page.pagePath.includes('Notes') && <GitalkComponent
+        options={{
+          clientID: "Ov23liBtIq74Cltq7uJQ",
+          clientSecret: 'e767cb1c64a63d6cc4f812bde6394836f02cee9c',
+          id: pageData.page.pagePath,
+          repo: "Lecture-Site",
+          owner: "wjsoj",
+          admin: ["wjsoj"],
+          title: pageData.page.title,
+        }}
+      /> 
+    }
+    </NoSSR>
   );
 }
