b63b68993223387d1b8d7a14710d653a0fd7b4d9
wjsoj <wjs@wjsphy.top>
2024-08-26 21:56:00 +0800

    fix: gitalk

--- src/components/GitalkCard.tsx
+++ src/components/GitalkCard.tsx
@@ -7,19 +7,20 @@ export default function GitalkCard() {
   const pageData = usePageData();
   return (
     <NoSSR>
-    {
-      pageData.page.pagePath.includes('Notes') && <GitalkComponent
-        options={{
-          clientID: "Ov23liBtIq74Cltq7uJQ",
-          clientSecret: 'e767cb1c64a63d6cc4f812bde6394836f02cee9c',
-          id: pageData.page.pagePath,
-          repo: "Lecture-Site",
-          owner: "wjsoj",
-          admin: ["wjsoj"],
-          title: pageData.page.title,
-        }}
-      /> 
-    }
+      {
+        pageData.page.pagePath.includes('Notes') && <GitalkComponent
+          options={{
+            clientID: "Ov23liBtIq74Cltq7uJQ",
+            clientSecret: 'e767cb1c64a63d6cc4f812bde6394836f02cee9c',
+            id: pageData.page.pagePath,
+            repo: "CourseCommunity",
+            owner: "StephenQSstarThomas",
+            labels: ["gitalk"],
+            admin: ["StephenQSstarThomas","wjsoj"],
+            title: "评论: "+pageData.page.pagePath,
+          }}
+        /> 
+      }
     </NoSSR>
   );
 }
